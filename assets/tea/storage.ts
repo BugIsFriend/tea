f5b8743be134c787c77f796950e8cc58f6b581d8
lishuan01_v <lishuan01_v@zuoyebang.com>
2025-12-06 16:14:51 +0800

    add: 添加存储加密

--- assets/tea/storage.ts
+++ assets/tea/storage.ts
@@ -2,60 +2,90 @@
  * 本地存储带过期时间的数据
  * 使用day 来定义数据格式
  */
+import { log, sys, warn } from "cc"
+import { DEBUG, PREVIEW } from "cc/env"
 
 export namespace storage {
+
+    const ALL_KEYS = '__ALL_KEYS'
+
     type StorageValue<T> = {
         value: T
         expireAt: number // 过期时间的时间戳（毫秒）
         encrypt?: boolean
     }
 
-    const ALL_KEYS = '__ALL_KEYS'
+    function encode<T>(obj: StorageValue<T>):string{ 
+        if (obj.encrypt && !PREVIEW) { 
+            // obj.value =  _encode(obj.value)  // TODO implement _encode mether 
+        }
+        return  JSON.stringify(obj)
+    }
 
-    export function set<T>(key: string, value: T, expireDate?: Dayjs, encrypt?:boolean) {
+    function decode<T>(content:string) { 
+        if (!!content) { 
+            let obj = JSON.parse(content) as StorageValue<T>
+            if (obj.encrypt && !PREVIEW) { 
+                // return _decode(obj.value) as T  // TODO implement _decode mether
+            }
+            return obj.value
+        }
+        return null
+    }
+
+    export function set<T>(key: string, value: T, expireDate?: Dayjs, encrypt?: boolean) {
+        
+        if (value === null || value === undefined) { 
+            warn('storage fail:  the value is null')
+            return 
+        }
+        
         const data: StorageValue<T> = {
             value,
             expireAt : expireDate?.valueOf(),
             encrypt : encrypt?true:false
         }
-        localStorage.setItem(key, JSON.stringify(data))
+        
+        sys.localStorage.setItem(key, encode(data))
 
-        let values = localStorage.getItem(ALL_KEYS) || ''
+        let values = sys.localStorage.getItem(ALL_KEYS) || ''
         if (!values.includes(key)) {
             values += `,${key}`
-            localStorage.setItem(ALL_KEYS, values)
+            sys.localStorage.setItem(ALL_KEYS, values)
         }
     }
 
     export function get<T>(key: string): T | null {
-        const item = localStorage.getItem(key)
-        if (!item) return null
+        const content:string = sys.localStorage.getItem(key)
+        if (!content) return null
         try {
-            const data: StorageValue<T> = JSON.parse(item)
+            const data: StorageValue<T> = decode(content)
             if (!!data.expireAt && Date.now() > data.expireAt) {
-                localStorage.removeItem(key)
+                remove(key)
                 return null
             }
             return data.value
         } catch {
-            localStorage.removeItem(key)
+            sys.localStorage.removeItem(key)
             return null
         }
     }
 
+    // 删除某个指定key
     export function remove(key: string) {
-        localStorage.removeItem(key)
-    }
-
-    export function dump(key){ 
-        
+        sys.localStorage.removeItem(key)
+        let keys_string: string = sys.localStorage.getItem(ALL_KEYS)
+        if (!!keys_string) { 
+            let allKeys = keys_string.split(',').filter((item) => item != key)
+            keys_string = allKeys.join(',')
+            sys.localStorage.getItem(keys_string) 
+        }
     }
 
     /**
      * 清除素有的key
      */
     export function clear() {
-        let allKeys: string = localStorage.getItem(ALL_KEYS)
-        if (!!allKeys) allKeys.split(',').forEach((key) => storage.remove(key))
+        sys.localStorage.clear()
     }
 }
